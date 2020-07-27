1608f96fb4b93acdf2583473b3d6bcd2ef45e9e6
Ainea Wabwoba <aineah28@gmail.com>
2020-07-27 13:33:17 +0300

    finished setting up the homepage

--- src/api/MockData.js
+++ src/api/MockData.js
@@ -1,22 +1,68 @@
+import Cabbage from "../assets/img/cabbage.jpg";
+
 export const products = [
   {
     id: 1,
-    name: "Cabbages",
     category: "vegetables",
-    linkUrl: "/shop/vegetables",
-    imgUrl: "../assets/img/cabbage.jpg",
+    routeName: "veg",
+    img: "https://i.ibb.co/3ymHnHk/assorted-vegetables.jpg",
+    items: [
+      {
+        id: 1,
+        name: "Cabbages",
+        sellerId: 1,
+        linkUrl: "/shop/vegetables",
+        imgUrl: Cabbage,
+      },
+      {
+        id: 2,
+        name: "Carrots",
+        sellerId: 1,
+        linkUrl: "/shop/vegetables",
+        imgUrl: "https://i.ibb.co/mG2cCZr/carrots.jpg",
+      },
+      {
+        id: 3,
+        name: "Kale",
+        sellerId: 1,
+        linkUrl: "/shop/vegetables",
+        imgUrl: "",
+      },
+    ],
   },
   {
     id: 2,
-    name: "Carrots",
-    category: "vegetables",
-    linkUrl: "/shop/vegetables",
-    imgUrl: "../assets/img/choose.svg",
+    category: "fruits",
+    routeName: "fruits",
+    img: "https://i.ibb.co/ggTvLTZ/sliced-fruits-on-tray-1132047.jpg",
+    items: [],
   },
   {
     id: 3,
-    name: "Kale",
-    category: "vegetables",
-    linkUrl: "../assets/img/choose.svg",
+    category: "cereals",
+    routeName: "cereals",
+    img: "https://i.ibb.co/3kV9Cys/brown-nut-lot-1192053.jpg",
+    items: [],
+  },
+];
+
+export const sellers = [
+  {
+    id: 1,
+    name: "Jane Doe",
+    profileImg: "",
+    profileLink: "",
+  },
+  {
+    id: 2,
+    name: "Alice",
+    profileImg: "",
+    profileLink: "",
+  },
+  {
+    id: 1,
+    name: "Janet",
+    profileImg: "",
+    profileLink: "",
   },
 ];
--- src/components/ProductPreview/ProductPreview.js
+++ src/components/ProductPreview/ProductPreview.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import { products } from "../../api/MockData";
 
@@ -13,21 +14,33 @@ class ProductPreview extends Component {
   render() {
     const { products } = this.state;
     return (
-      <div className="bg-orange-200">
+      <div className="bg-orange-100 pb-20">
         <div className="w-4/5 mx-auto">
-          <h2 className="pt-20 text-center">
-            Start your journey with us today
+          <h2 className="pt-20 text-center font-semibold text-4xl">
+            Featured categories
           </h2>
           <div className="flex flex-wrap mx-1 overflow-hidden font-light mt-12">
-            {products.map(({ id, imgUrl, name }) => (
+            {products.map(({ id, img, category, routeName }) => (
               <div
                 key={id}
-                className="my-1 px-1 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3"
+                className="my-1 px-4 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3"
                 // style={{ backgroundImage: `url(${imgUrl})` }}
-                onClick={() => console.log(imgUrl)}
+                onClick={() => console.log(img)}
               >
-                <img src="{imgUrl}" alt="" className="w-100" />
-                <h3>{name}</h3>
+                <img
+                  src={img}
+                  alt=""
+                  className="w-100 max-h-full rounded shadow-lg"
+                />
+                <div className="flex justify-between p-6 items-center">
+                  <h3 className="font-medium capitalize">{category}</h3>
+                  <Link
+                    to={`/${routeName}`}
+                    className="bg-teal-500 py-2 px-8 rounded-full text-white mr-2 hover:text-teal-500 hover:bg-transparent border hover:border-teal-500"
+                  >
+                    Shop
+                  </Link>
+                </div>
               </div>
             ))}
           </div>
