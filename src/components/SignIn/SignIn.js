6f1ae21a16f2011070b77ae3566b6528d21a8174
Ainea Wabwoba <aineah28@gmail.com>
2020-08-08 17:04:59 +0300

    modified sign in and sign up components

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import TextInput from "../TextInput/TextInput";
+// import TextInput from "../TextInput/TextInput";
 import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends Component {
@@ -23,48 +23,100 @@ class SignIn extends Component {
   };
 
   render() {
+    const { email, password } = this.state;
     return (
-      <div>
-        <div className="w-full bg-grey-lightest pt-16">
-          <div className="container mx-auto py-8">
-            <div className="w-5/6 lg:w-1/2 mx-auto bg-white">
-              <div className="py-4 px-8 text-black text-xl  border-grey-lighter">
-                Sign in to your account
-              </div>
-              <form onSubmit={this.handleSubmit} className="py-4 px-4">
-                <TextInput
-                  label="Enter your email Address"
-                  type="email"
-                  placeholder="Your Email address"
-                  onChange={this.handleChange}
-                />
-                <TextInput
-                  label="Enter your password"
-                  type="password"
-                  placeholder="password"
-                  onChange={this.handleChange}
-                />
-
-                <p className="text-grey text-xs mt-1">At least 6 characters</p>
-                <div className="md:content-center md:justify-between mt-8">
-                  <button
-                    className="bg-transparent border border-teal-400 py-2 px-8 hover:bg-teal-500 rounded-full hover:text-white mb-4"
-                    type="submit"
-                  >
-                    Sign In
-                  </button>
-
-                  <button
-                    className="bg-transparent border border-teal-400 py-2 px-8 hover:bg-teal-500 rounded-full hover:text-white mb-4"
-                    type="submit"
-                    onClick={signInWithGoogle}
-                  >
-                    Sign In With Google
-                  </button>
-                </div>
-              </form>
-            </div>
+      <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div class="max-w-md w-full">
+          <div>
+            <h2 class="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
           </div>
+          <form class="mt-8" onSubmit={this.handleSubmit}>
+            <input type="hidden" name="remember" value="true" />
+            <div class="rounded-md shadow-sm">
+              <div>
+                <input
+                  aria-label="Email address"
+                  name="email"
+                  type="email"
+                  value={email}
+                  onChange={this.handleChange}
+                  required
+                  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  placeholder="Email address"
+                />
+              </div>
+              <div class="-mt-px">
+                <input
+                  aria-label="Password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={this.handleChange}
+                  required
+                  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div class="mt-6 flex items-center justify-between">
+              <div class="flex items-center">
+                <input
+                  id="remember_me"
+                  type="checkbox"
+                  class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+                />
+                <label
+                  for="remember_me"
+                  class="ml-2 block text-sm leading-5 text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              <div class="text-sm leading-5">
+                <a
+                  href="/#"
+                  class="font-light text-teal-500 hover:text-teal-400 focus:outline-none focus:underline transition ease-in-out duration-150"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+            </div>
+
+            <div class="mt-6">
+              <button
+                type="submit"
+                class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-teal-500 hover:bg-teal-400 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+              >
+                <span class="absolute left-0 inset-y-0 flex items-center pl-3"></span>
+                Sign in
+              </button>
+            </div>
+          </form>
+          <p class="mt-2 text-center text-sm leading-5 text-gray-600">Or</p>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+            onClick={signInWithGoogle}
+          >
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <svg
+                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400 transition ease-in-out duration-150"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+            Sign in with Google
+          </button>
         </div>
       </div>
     );
--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -2,15 +2,9 @@ import React from "react";
 
 function TextInput({ handleChange, label, id, required, ...otherProps }) {
   return (
-    <div className="mb-4">
-      <label
-        className="block text-grey-darker text-sm font-bold mb-2"
-        htmlFor={id}
-      >
-        {label}
-      </label>
+    <div className="">
       <input
-        className="appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
         onChange={handleChange}
         required
         {...otherProps}
